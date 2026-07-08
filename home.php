7611bf860830a6d98e38faab785e996079ff3633
Mar Ul Hamda <marulhamda2708@gmail.com>
2026-07-08 12:14:52 +0700

    ubah judul

--- home.php
+++ home.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>Sistem Pengelolaan Data Produk UMKM</title>
+  <title>Sistem Pengelolaan Data Produk UMKMa</title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css" rel="stylesheet">
   <link href="style.css" rel="stylesheet">
