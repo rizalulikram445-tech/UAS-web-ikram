8ed3e2c4ce2c856b97d248c878e8bf78c715d660
Rizalul Ikram <rizalulikram445@gmail.com>
2026-07-09 12:37:45 +0700

    title umkm kami

--- home.php
+++ home.php
@@ -7,7 +7,7 @@ include "koneksi.php";
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>Sistem Pengelolaan Data Produk UMKM</title>
+  <title>Sistem Pengelolaan Data Produk UMKM kami</title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css" rel="stylesheet">
   <link href="style.css" rel="stylesheet">
