3247fdfba426218e6e5a421bda47faec4764ff3f
Mar Ul Hamda <marulhamda2708@gmail.com>
2026-07-09 14:54:40 +0700

    perbaiki koneksi database

--- koneksi.php
+++ koneksi.php
@@ -6,7 +6,7 @@ $db   = "db_umkm_produk"; // Sesuaikan dengan nama database di phpMyAdmin
 
 $koneksi = mysqli_connect($host, $user, $pass, $db);
 
-if ($koneksi){
-    die("Koneksi gagal : ".mysqli_connect_error())
+if (!$koneksi){
+    die("Koneksi gagal : ".mysqli_connect_error());
 }
 ?>
